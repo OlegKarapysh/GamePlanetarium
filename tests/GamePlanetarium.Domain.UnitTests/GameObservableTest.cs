1efe8cbcb7c28750f7003d686921ec32221e2181
Oleh Karapysh <sebastianvell777@gmail.com>
2023-06-06 01:35:08 +0300

    Added unit tests for game seed logic

--- tests/GamePlanetarium.Domain.UnitTests/GameObservableTest.cs
+++ tests/GamePlanetarium.Domain.UnitTests/GameObservableTest.cs
@@ -1,12 +1,12 @@
 ﻿using GamePlanetarium.Domain.Answer;
 using GamePlanetarium.Domain.Game;
+using GamePlanetarium.Domain.GameSeeds;
 using GamePlanetarium.Domain.Question;
 
 namespace GamePlanetarium.Domain.UnitTests;
 
 public class GameObservableTest
 {
-    private readonly QuestionImage _questionImage;
     private readonly Answer.Answer[] _answers1;
     private readonly Answer.Answer[] _answers2;
     private readonly IQuestion[] _questions;
@@ -14,7 +14,7 @@ public class GameObservableTest
 
     public GameObservableTest()
     {
-        _questionImage = new QuestionImage("QuestionImage1", new byte[] { 1, 2 }, new byte[] { 2, 1 });
+        var questionImage = new QuestionImage("QuestionImage1", new byte[] { 1, 2 }, new byte[] { 2, 1 });
         _answers1 = new Answer.Answer[]
         {
             new("answer1", Answers.First, false),
@@ -29,8 +29,8 @@ public class GameObservableTest
         };
         _questions = new IQuestion[]
         {
-            new SingleAnswerQuestion("question1", _answers1, _questionImage),
-            new SingleAnswerQuestion("question1", _answers2, _questionImage)
+            new SingleAnswerQuestion("question1", _answers1, questionImage),
+            new SingleAnswerQuestion("question1", _answers2, questionImage)
         };
         _sut = new GameObservable(_questions);
     }
@@ -138,5 +138,54 @@ public class GameObservableTest
         monitoredSut!.Should()!.Raise("GameEnded")!
                      .WithSender(_sut)!
                      .WithArgs<EventArgs>();
+    }
+
+    [Fact]
+    public void GetCorrectAnswers_ShouldReturnArrayOfCorrectAnswers()
+    {
+        // Assign.
+        var correctAnswers = new[] { Answers.Second, Answers.First };
+        
+        // Act.
+        var actualAnswers = _sut.GetCorrectAnswers();
+        
+        // Assert.
+        actualAnswers.Should()!.Equal(correctAnswers);
+    }
+
+    [Fact]
+    public void GetQuestionTextSeed_ShouldReturnQuestionTextSeed()
+    {
+        // Act.
+        var actualQuestionTextSeed = _sut.GetQuestionTextSeed();
+        
+        // Assert.
+        actualQuestionTextSeed.Data.Should()!.HaveCount(_questions.Length);
+        actualQuestionTextSeed.Data[0].QuestionText.Should()!.Be(_questions[0].Text);
+        actualQuestionTextSeed.Data[1].QuestionText.Should()!.Be(_questions[1].Text);
+        actualQuestionTextSeed.Data[0].AnswersText.Should()!.HaveCount(_answers1.Length);
+        actualQuestionTextSeed.Data[1].AnswersText.Should()!.HaveCount(_answers2.Length);
+        actualQuestionTextSeed.Data[0].AnswersText[0].Should()!.Be(_answers1[0].Text);
+        actualQuestionTextSeed.Data[1].AnswersText[0].Should()!.Be(_answers2[0].Text);
+    }
+
+    [Fact]
+    public void ChangeQuestionsTextBySeed_ShouldChangeQuestionsAndAnswersTextInGame()
+    {
+        // Assign.
+        var seed = new QuestionTextSeed(new QuestionTextData[]
+        {
+            new("newQuestion1", new[] { "newAnswer1", "newAnswer2", "newAnswer3" }),
+            new("newQuestion2", new[] { "newAnswer4", "newAnswer5", "newAnswer6" })
+        });
+        
+        // Act.
+        _sut.ChangeQuestionsTextBySeed(seed);
+        
+        // Assert.
+        _sut.Questions[0].Text.Should()!.Be(seed.Data[0].QuestionText);
+        _sut.Questions[1].Text.Should()!.Be(seed.Data[1].QuestionText);
+        _sut.Questions[0].Answers[0].Text.Should()!.Be(seed.Data[0].AnswersText[0]);
+        _sut.Questions[1].Answers[1].Text.Should()!.Be(seed.Data[1].AnswersText[1]);
     }
 }
